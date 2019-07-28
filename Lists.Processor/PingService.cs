59ca2d7de3d66ec34ac037014687e8c7441bfb1f
taybrown <taybrown>
2019-07-28 09:14:50 -0400

    Adding CacheClient to PingService

--- Lists.Processor/PingService.cs
+++ Lists.Processor/PingService.cs
@@ -1,8 +1,29 @@
 
+using Microsoft.Extensions.Logging;
+using Lists.Processor.Caching;
+
 namespace Lists.Processor
 {
-    public class PingService : IntervalService
+    public class ServiceInfo { }
+    public class PingService : IntervalService<ServiceInfo>
     {
+        private readonly ICachingClient _cacheClient;
+        public PingService(ILogger<PingService> logger, ICachingClient cacheClient) 
+            : base(nameof(PingService), 0, 1000, logger) 
+            {
+                _cacheClient = cacheClient;
+            }
+
+        protected override void ExecuteAsync(ServiceInfo trigger)
+        {
+            _logger.LogInformation("ping");
+            _cacheClient.Ping();
+        }
+
+        public override void Stop() 
+        {
+            base.Stop();
+        }
     }
 
 }
